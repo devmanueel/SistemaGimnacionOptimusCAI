1e8edca0cf4eb01b6c6afeda29aec56a0ad69296
Joako-G <gallardojoako@gmail.com>
2026-05-12 09:53:50 -0300

    style(General): Modificar el diseño de los buscadores y algunos ComboBox para usar el diseño global creado

--- Paginas/TurnosPage.xaml.cs
+++ Paginas/TurnosPage.xaml.cs
@@ -61,7 +61,7 @@ namespace SistemaGimnacionOptimusCAI.Paginas
                 // Agregar opción "sin asignar" al inicio
                 var lista = new List<Usuario>
                 {
-                    new Usuario { Id = 0, Nombre = "Sin", Apellido = "asignar" }
+                    new Usuario { Id = 0, Nombre = "asignar", Apellido = "Sin" }
                 };
                 lista.AddRange(usuarios);
                 cmbInstructor.ItemsSource = lista;
